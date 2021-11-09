b79d354f3b2ba45e64a001744783dea161539608
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-09 17:51:07 -0500

    prueba con hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react'
 import './App.css';
+//import {fbConfig} from './fb'
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {Register} from './components/Register';
 import {Login} from './components/Login';
+import {Home} from './components/Home'
 
 
 
 export const App= () => {
+
+  //const [user, setUser] = React.useState(null)
+  //console.log(user);
+  //console.log(setUser)
+  /*setUser = {setUser}*/
+
   return (
+    
     <div className="App">
+      
     <Router>
       <Switch>
         <Route exact path= "/" >
@@ -21,6 +32,11 @@ export const App= () => {
         <Route path = "/register">
          <Register />
         </Route>
+        
+        <Route path = "/home">
+         <Home />
+        </Route>
+
       </Switch>
 
     </Router>
