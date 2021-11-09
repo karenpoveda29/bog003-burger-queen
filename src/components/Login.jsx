31c5782a8e46eeeab314614b0ee39272d1fb9ac7
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-09 17:53:26 -0500

    firebase auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import {fbConfig} from "../fb"
+import './Auth.css'
+//import { Register } from "./Register"
 
 
-export const Login = ()=> {
+
+export const Login = (props)=> {
+
+        
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const email = e.target.inputEmail.value;
+        const password = e.target.inputPassword.value;
+    
+        console.log(email,password);
+
+        fbConfig.auth().signInWithEmailAndPassword(email,password)
+        .then((userFb)=>{
+            console.log('Sesión iniciada correctamente', userFb);
+           //console.log(props.setUser(userFb))
+        })
+        
+    }
     return (
-        <div>
-            <img src={"./images/logo.png"} alt="Burger Queen" />
+        <div className="Login">
+            <img className= "Login-img" src={"./images/logo.png"} alt="Burger Queen" />
             <h1>Inicia sesión</h1>
-
-            <input type="email" class="input-email" placeholder="email"></input>
-            <input type="password" class="input-password" placeholder="password"></input>
+            <form className= "form" onSubmit = {submitHandler}>
+            <input type="email" id="inputEmail" placeholder="email"></input>
+            <input type="password" id="inputPassword" placeholder="password"></input>
+            {/*<>{user ? <Home /> : <Register/>}</>*/}
+            {/*<Link to = '/home'>*/}
             <button>Ingresar</button>
+            {/*</Link>*/}
+            </form>
             <Link to="/register">Registrarse</Link>
 
         </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,20 +1,43 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import {fbConfig} from "../fb"
+import './Auth.css'
 
 
-export const Register = () => {
+export const Register = (props) => {
+ 
+    
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const user = e.target.userType.value
+        const name = e.target.inputName.value
+        const email = e.target.inputEmail.value;
+        const password = e.target.inputPassword.value;
+    
+        console.log(user,name,email,password);
+
+        fbConfig
+        .auth()
+        .createUserWithEmailAndPassword(email,password)
+        .then((userFb)=>{
+            console.log('Ya puedes iniciar sesión con', userFb.user.email)
+        })
+    }
+
     return (
-        <div>
-            <img src={"./images/logo-image.png"} alt="Burger Queen" />
+        <div className="Login">
+            <img className="Register-img" src={"./images/logo-image.png"} alt="Burger Queen" />
             <h1>Registro</h1>
-            <select>
-                <option value="mesero">Mesero</option>
-                <option value="jefe-cocina">Jefe de cocina</option>
+            <form className="form" onSubmit = {submitHandler}>
+            <select id= "userType">
+                <option value="Mesero">Mesero</option>
+                <option value="Jefe_cocina">Jefe de cocina</option>
             </select>
-            <input type="text" class="input-name" placeholder="name"></input>
-            <input type="email" class="input-email" placeholder="email"></input>
-            <input type="password" class="input-password" placeholder="password"></input>
+            <input type="text" id="inputName" placeholder="name"></input>
+            <input type="email" id="inputEmail" placeholder="email"></input>
+            <input type="password" id="inputPassword" placeholder="password"></input>
             <button>Registrarse</button>
+            </form>
             <Link to="/login">¿Ya tienes cuenta? Inicia sesión</Link>
 
         </div>
