f66f5d78b839252b3158836165280d5c189a2538
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-09 10:48:18 -0500

    configuracion firebase

---
+++ src/fb.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+
+export const fbConfig = firebase.initializeApp({
+    "projectId": "bq10-burger-queen",
+    "appId": "1:234202997381:web:7ee31649837f446749bfec",
+    "storageBucket": "bq10-burger-queen.appspot.com",
+    "locationId": "southamerica-east1",
+    "apiKey": "AIzaSyDnCHpHBppz-DTRlg1Ok_azolSOSlqocW4",
+    "authDomain": "bq10-burger-queen.firebaseapp.com",
+    "messagingSenderId": "234202997381"
+});
