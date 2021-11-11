52250dd7e82f6043ea6e944e0635be0287316616
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-11-11 11:42:38 -0500

    borrar código comentado

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React from 'react'
 import './App.css';
-//import {fbConfig} from './fb'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { RegisterForm } from './components/RegisterForm';
 import { Login } from './components/Login';
 import { Home } from './components/Home'
 
-
-
 export const App= () => {
-
-  //const [user, setUser] = React.useState(null)
-  //console.log(user);
-  //console.log(setUser)
-  /*setUser = {setUser}*/
 
   return (
     
     <div className="App">
       
-    <Router>
-      <Switch>
-        <Route exact path= "/">
-         <Login />
-        </Route>
+      <Router>
+        <Switch>
+          <Route exact path= "/">
+            <Login />
+          </Route>
 
-        <Route path = "/login">
-         <Login />
-        </Route>
-        
-        <Route path = "/register">
-         <RegisterForm />
-        </Route>
-        
-        <Route path = "/home">
-         <Home />
-        </Route>
+          <Route path = "/login">
+            <Login />
+          </Route>
+          
+          <Route path = "/register">
+            <RegisterForm />
+          </Route>
+          
+          <Route path = "/home">
+            <Home />
+          </Route>
 
-      </Switch>
+        </Switch>
 
-    </Router>
+      </Router>
     </div>
   );
 }
