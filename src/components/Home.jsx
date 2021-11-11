8761979daf8256f5c2908d3b779bc6ee29670f0b
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-11-11 11:44:49 -0500

    agregar logo a la vista home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React  from "react";
 
-export const Home = () =>{
+export const Home = (props) =>{
 
     return (
         <div>
-          <h1>Bienvenido a Burger Queen</h1>
+          <h1>{props.title}</h1>
+          <img className= "logo-home" src={"./images/logo.png"} alt="Burger Queen" />
         </div>
     )
 }
