c645c95fd0b7aa8e6f49a8e8445e7b2822ba442c
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-11-11 11:46:45 -0500

    borrar función fb y hacer redirección a home

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,36 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import {fbConfig} from "../fb"
+import { loginUser } from "../firebaseFunctions"
 import './Auth.css'
 //import { Register } from "./Register"
 
 
 
 export const Login = (props)=> {
-
-        
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const email = e.target.inputEmail.value;
-        const password = e.target.inputPassword.value;
     
-        console.log(email,password);
+	const loginSession = (e) => {
+		e.preventDefault();
+		const email = e.target.inputEmail.value;
+		const password = e.target.inputPassword.value;
+	
+		loginUser(email, password)
+		window.location.pathname = "/home";
+			
+	}
 
-        fbConfig.auth().signInWithEmailAndPassword(email,password)
-        .then((userFb)=>{
-            console.log('Sesión iniciada correctamente', userFb);
-           //console.log(props.setUser(userFb))
-        })
-        
-    }
-    return (
-        <div className="Login">
-            <img className= "Login-img" src={"./images/logo.png"} alt="Burger Queen" />
-            <h1>Inicia sesión</h1>
-            <form className= "form" onSubmit = {submitHandler}>
-            <input type="email" id="inputEmail" placeholder="email"></input>
-            <input type="password" id="inputPassword" placeholder="password"></input>
-            {/*<>{user ? <Home /> : <Register/>}</>*/}
-            {/*<Link to = '/home'>*/}
-            <button>Ingresar</button>
-            {/*</Link>*/}
-            </form>
-            <Link to="/register">Registrarse</Link>
+	return (
+			<div className="Login">
+					<img className= "Login-img" src={"./images/logo.png"} alt="Burger Queen" />
+					<h1>Inicia sesión</h1>
+					<form className= "form" onSubmit = { loginSession }>
+					<input type="email" id="inputEmail" placeholder="email"></input>
+					<input type="password" id="inputPassword" placeholder="password"></input>
+					
+					<button>Ingresar</button>
+					
+					</form>
+					<Link to="/register">Registrarse</Link>
 
-        </div>
-    )
+			</div>
+	)
 }
