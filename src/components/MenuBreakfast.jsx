9287e2b0b1d8e076b2ecf36f0c1c3a3644efc9c4
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-15 20:09:57 -0500

    prueba para acceder a data de archivo json

--- src/components/MenuBreakfast.jsx
+++ src/components/MenuBreakfast.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-//const data = require("../data/menu.json");
+import data from '../data/menu.json';
 import '../styles/menuBreakfast.css'
+
+
+//  const breakfast = data.filter((item)=> item.type === "desayuno")
+//const breakfast = data[0].desayuno.productos.filter((item)=> item.item)
+const breakfast = data[0].desayuno.productos
+const nameBreakfast = breakfast.map((item)=> `producto: ${item.item} precio: ${item.precio} ` )
+console.log(nameBreakfast)
 
 export const MenuBreakfast = () => {
     return (
@@ -9,10 +16,9 @@ export const MenuBreakfast = () => {
           <label> <input type="checkbox" />Café con leche $7</label><br/>
           <label> <input type="checkbox" />Sandwich de jamón y queso $10</label><br/>
           <label> <input type="checkbox" />Jugo de frutas natural $7</label><br/>
-          <div className="line">
-          <hr />
-          </div>
-          
+          {/* <div className="line"> */}
+          {/* <hr /> */}
+          {/* </div>           */}
 
         </div>
     )
