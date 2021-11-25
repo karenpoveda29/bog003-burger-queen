c7cd39bda1bd1fb5f5a343fc5a9d61b2cf74db86
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-25 17:03:45 -0500

    se crea hook para input de mesa y nombre

--- src/components/waiter/MenuTitle.jsx
+++ src/components/waiter/MenuTitle.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/menuTitle.css";
 
 export const MenuTitle = ({ title }) => {
 
+  const customerData = {
+    table:'',
+    name:''
+  }
+  const [customer, setCostumer] = useState({customerData})
+
+  const handleData =(e)=>{
+
+      //  const {name, value} = e.target
+      //  console.log(name, value);
+
+    //const customerData = {table: e.target.tagName === '', name: e.target.value}
+    console.log(e.target);
+    //setCostumer(customerData)
+    // console.log(newCustomer);
+    //console.log(setCostumerData(newCustomer));
+    
+    // e.preventDefault();
+    // console.log(e.target.value);
+  }
+  console.log(customer)
+
   return (
     <div className="menuTitle">
        <h1>{ title }</h1>
-  
-      <select className="user-table" id="userTable">
+      
+      <select className="user-table" id="userTable" name="table" onChange = {handleData}>
         <option value="mesa">Mesa</option>
         <option value="1">Mesa 1</option>
         <option value="2">Mesa 2</option>
@@ -25,7 +47,10 @@ export const MenuTitle = ({ title }) => {
         id="inputNameClient"
         placeholder="Nombre y apellido"
         required
+        name="name"
+        onChange = {handleData}
       ></input>
+      
     </div>
   );
 };
