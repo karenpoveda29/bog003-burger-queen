6f404f6410e2a537e26271bbeed9a8c0c392d10d
Maria-Camila123 <na01alvarez@gmail.com>
2021-11-26 14:06:19 -0500

    función que crea colección con la info del cliente

--- src/components/waiter/OrdersButtons.jsx
+++ src/components/waiter/OrdersButtons.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { createOrder } from "../../firebaseFunctions";
 
-export function OrdersButtons( {cancelButton, itemsOrder} ) {
+export function OrdersButtons( {cancelButton, itemsOrder, customer} ) {
+
+  const handleSendKitchen = (customerInfo) =>{
+
+    createOrder(customerInfo.table, customerInfo.clientName)
+  }
+
   return (
     <div className= "btn-waiter">
       <button className="btn-cancel" onClick={() => cancelButton(itemsOrder)}>Cancelar pedido</button>
-      <button className="btn-sendKitchen" type="submit">Enviar a cocina</button>
+      <button className="btn-sendKitchen" onClick={() =>handleSendKitchen(customer)} >Enviar a cocina</button>
     </div>
   )
 }
