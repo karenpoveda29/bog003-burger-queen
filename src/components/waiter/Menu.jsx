09d1fa6b00dcae636e4efd90cb664dce742e3aa1
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-12-02 10:45:33 -0500

    reestructurar el renderizado del componente menu

--- src/components/waiter/Menu.jsx
+++ src/components/waiter/Menu.jsx
@@ -1,55 +1,53 @@
 import React from "react";
 import "../../styles/menu.css";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 //Este componente crea los items del menú
 
-export const Menu = ({ menuProducts, type, summaryProducts, setSummaryProducts, hamburgerType, setShowModalWindow}) => { //array de objetos con los items de uno de los menús: lunch o breakfast
+export const Menu = ({ menuProducts, type, summaryProducts }) => {
+  //array de objetos con los items de uno de los menús: lunch o breakfast
 
-  const handlerSelectProduct = (e)=>{
-    const item = summaryProducts.find(product => product.item === e.target.innerText)
-    if(!item){
-      
-      const newSummary = [...summaryProducts]
-      const newItem = {id:uuidv4(), item: `${e.target.innerText} ${hamburgerType}`, price: 0, quantity: 1, categories: []}
-      
-      menuProducts.forEach(product => {
-        if(product.item === e.target.innerText){
-          newItem.price = product.price
-          newItem.categories = product.categories
+  const handlerSelectProduct = (e) => {
+    const item = summaryProducts.find(
+      (product) => product.item === e.target.innerText
+    );
+    if (!item) {
+      const newSummary = [...summaryProducts];
+      const newItem = {
+        id: uuidv4(),
+        item: `${e.target.innerText}`,
+        price: 0,
+        quantity: 1,
+        categories: [],
+      };
+
+      menuProducts.forEach((product) => {
+        if (product.item === e.target.innerText) {
+          newItem.price = product.price;
+          newItem.categories = product.categories;
         }
       });
-      
-      if(e.target.innerText.includes("Hamburguesa")){
-        setShowModalWindow(true)
-        newSummary.push(newItem)
-        setSummaryProducts(newSummary)
-        console.log(newItem)
-      }else{
-        newSummary.push(newItem)
-        setSummaryProducts(newSummary)
-      }
 
-      
+      if (e.target.innerText.includes("Hamburguesa")) {
+        newSummary.push(newItem);
+      } else {
+        newSummary.push(newItem);
+      }
     }
   };
 
   return (
     <div className="menu">
-      <h2>{ type }</h2>
+      <h2>{type}</h2>
       <ul>
-        {menuProducts.filter(product => product.type === type).map( product => (
-          <li key={ product.id }>
-            
+        {menuProducts.map((product) => (
+          <li key={product.id}>
             {/* <input type="checkbox" value = { product.item } onChange={handlerChange}/> */}
             <p onClick={handlerSelectProduct}>{product.item}</p>
             <p>$ {product.price}</p>
-            
           </li>
-        ))}        
+        ))}
       </ul>
     </div>
   );
 };
-
--- src/views/sub-views/TakingOrders.jsx
+++ src/views/sub-views/TakingOrders.jsx
@@ -1,61 +1,51 @@
-import React, { useState } from "react"
-import { MenuTitle } from "../../components/waiter/MenuTitle"
-import { Menu } from "../../components/waiter/Menu"
-import { OrderSummary } from "../../components/waiter/OrderSummary"
-import { TotalOrder } from "../../components/waiter/TotalOrder"
-import { OrdersButtons } from "../../components/waiter/OrdersButtons"
-import { BurgerModal } from '../../components/waiter/BurgerModal'
+import React, { useState } from "react";
+import { MenuTitle } from "../../components/waiter/MenuTitle";
+import { Menu } from "../../components/waiter/Menu";
+import { OrderSummary } from "../../components/waiter/OrderSummary";
+import { TotalOrder } from "../../components/waiter/TotalOrder";
+import { OrdersButtons } from "../../components/waiter/OrdersButtons";
+/* import { BurgerModal } from '../../components/waiter/BurgerModal' */
 
-export const TakingOrders = 
-    ({ title, 
-      menuProducts, 
-      category, 
-      summaryProducts,
-      setSummaryProducts,
-      onIncrease, onDecrease, onDelete,
-      total 
-    }) => {
+export const TakingOrders = ({
+  title,
+  menuProducts,
+  summaryProducts,
+  onIncrease,
+  onDecrease,
+  onDelete,
+  total,
+}) => {
+  const [customer, setCustomer] = useState({
+    table: "",
+    clientName: "",
+  });
 
-    const [customer, setCustomer] = useState({
-      table:'',
-      clientName:''
-    })
+  const menuTypes = [...new Set(menuProducts.map((product) => product.type))];
 
-    
-    const [hamburgerType, sethamburgerType] = useState("");
-    
-    const [showModalWindow, setShowModalWindow] = useState(false)
-    
-    return (
-      <main className="order-block">
-        <MenuTitle title = { title } customer={ customer } setCustomer = { setCustomer } />
-        {showModalWindow === true && <BurgerModal sethamburgerType={ sethamburgerType } setShowModalWindow={ setShowModalWindow }/>}
-        {category === "breakfast" 
-          ? 
-            <div>
-                <Menu menuProducts={ menuProducts } summaryProducts={ summaryProducts } setSummaryProducts = { setSummaryProducts } type="Bebidas"/>
-                <Menu menuProducts={ menuProducts } summaryProducts={ summaryProducts } setSummaryProducts = { setSummaryProducts } type="Sandwiches"/>
-            </div>
-          : 
-            <div>
-                <Menu menuProducts={ menuProducts } summaryProducts={ summaryProducts } setSummaryProducts = { setSummaryProducts } hamburgerType={ hamburgerType } setShowModalWindow={ setShowModalWindow } type="Bebidas"/>
-                <Menu menuProducts={ menuProducts } summaryProducts={ summaryProducts } setSummaryProducts = { setSummaryProducts } hamburgerType={ hamburgerType } setShowModalWindow={ setShowModalWindow } type="Hamburguesas"/>
-                <Menu menuProducts={ menuProducts } summaryProducts={ summaryProducts } setSummaryProducts = { setSummaryProducts } hamburgerType={ hamburgerType } setShowModalWindow={ setShowModalWindow } type="Acompañamientos"/>
-            </div> 
-        }
-        <OrderSummary 
-          summaryProducts = { summaryProducts } 
-          onDecrease={ onDecrease } 
-          onIncrease={ onIncrease } 
-          onDelete={ onDelete } 
+  return (
+    <main className="order-block">
+      <MenuTitle title={title} customer={customer} setCustomer={setCustomer} />
+
+      {menuTypes.map((type) => (
+        <Menu
+          menuProducts={menuProducts.filter((product) => product.type === type)}
+          type={type}
+          summaryProducts={summaryProducts}
         />
-        { summaryProducts.length >=  1 
-        ? 
-          <TotalOrder total={ total }/> 
-        : 
-          <TotalOrder total={ 0 }/>
-        }
-        <OrdersButtons customer={ customer } summaryProducts = { summaryProducts }/>
-      </main>
-    );
+      ))}
+
+      <OrderSummary
+        summaryProducts={summaryProducts}
+        onDecrease={onDecrease}
+        onIncrease={onIncrease}
+        onDelete={onDelete}
+      />
+      {summaryProducts.length >= 1 ? (
+        <TotalOrder total={total} />
+      ) : (
+        <TotalOrder total={0} />
+      )}
+      <OrdersButtons customer={customer} summaryProducts={summaryProducts} />
+    </main>
+  );
 };
